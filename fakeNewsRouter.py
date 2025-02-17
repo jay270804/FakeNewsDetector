231955cceb6b571bd033109acc93ade03009ea2e
jay270804 <jaypatel270804@gmail.com>
2025-02-17 19:28:30 +0530

    fix: fakeNewsRouter.py

--- fakeNewsRouter.py
+++ fakeNewsRouter.py
@@ -1,5 +1,5 @@
 from http import HTTPStatus
-import constants
+# import constants
 from fastapi.responses import JSONResponse
 from database import get_db
 from sqlalchemy.orm import Session
@@ -7,8 +7,22 @@ from fastapi import Depends, FastAPI, HTTPException
 from getVerifiedFakeNewsRepository import get_verified_news_data, store_fake_news_data
 from schemas import VerifiedFakeNewsResponse, NewsRequest, NewsDetectionResponse
 from final_pipeline import final_pipeline
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # Your frontend URL
+    "http://localhost:5173/search",  # Your frontend URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/me")
 def read_root():
@@ -42,10 +56,10 @@ def detect_fake_news(req: NewsRequest, db: Session = Depends(get_db)):
     try:
         response = final_pipeline(text)
         # return
-        print("Before veracity check")
-        if response['veracity'] == constants.FALSE:
-            print("inside vercity check")
-            store_fake_news_data(db=db,news_title=text, confidence=response['credibility'])
+        # print("Before veracity check")
+        # if response['veracity'] == constants.FALSE:
+        #     print("inside vercity check")
+        #     store_fake_news_data(db=db,news_title=text, confidence=response['credibility'])
         return NewsDetectionResponse(
             veracity=response['veracity'],
             credibility=response['credibility'],
